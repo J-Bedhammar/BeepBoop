e6a359eb66d73109ae63ee1e4118c43f993a9755
Jennifer Bedhammar <jennifer95@hotmail.com>
2017-09-21 14:21:03 +0200

    projektfiler

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody2D rig_;
+    MicrophoneInput micInput;
+
+	// Use this for initialization
+	void Start () {
+        rig_ = GetComponent<Rigidbody2D>();
+        micInput = FindObjectOfType<MicrophoneInput>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(micInput.GetFrequency());
+
+        if (micInput.GetFrequency() >= 300 && micInput.GetFrequency() < 500)
+		    rig_.MovePosition(transform.position + new Vector3(micInput.GetFrequency() /-1000, 0, 0));
+        else if (micInput.GetFrequency() >= 500 && micInput.GetFrequency() < 850)
+            rig_.MovePosition(transform.position + new Vector3(micInput.GetFrequency() /1500, 0, 0));
+        else if (micInput.GetFrequency() >= 850)
+            rig_.MovePosition(transform.position + new Vector3(0, micInput.GetFrequency() /2800, 0));
+        else
+            rig_.MovePosition(transform.position + new Vector3(0, 0, 0));
+
+    }
+}
