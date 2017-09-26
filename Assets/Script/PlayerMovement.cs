946a9653d4e058ad214a714a9279209a7ea56a41
Jennifer Bedhammar <jennifer95@hotmail.com>
2017-09-26 14:05:17 +0200

    plz work

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -17,11 +17,11 @@ public class PlayerMovement : MonoBehaviour {
 	void Update () {
         print(micInput.GetFrequency());
         
-        if (micInput.GetFrequency() >= 100 && micInput.GetFrequency() < 200)
+        if (micInput.GetFrequency() >= 100 && micInput.GetFrequency() < 220)
 			rig_.MovePosition(transform.position + new Vector3(-1, 0, 0)); //micInput.GetFrequency() /-1000
-        else if (micInput.GetFrequency() >= 200 && micInput.GetFrequency() < 300)
+        else if (micInput.GetFrequency() >= 250 && micInput.GetFrequency() < 400)
 			rig_.MovePosition(transform.position + new Vector3(1, 0, 0)); //micInput.GetFrequency() /1500
-        else if (micInput.GetFrequency() >= 300)
+        else if (micInput.GetFrequency() >= 400)
 			rig_.MovePosition(transform.position + new Vector3(0, 1, 0)); //micInput.GetFrequency() /280
     }
 }
