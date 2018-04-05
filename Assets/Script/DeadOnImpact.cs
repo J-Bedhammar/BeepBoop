6e0c2eefc88243e926a710cf6251fe6d4fbf612c
Sparvolle <Oliverj96@hotmail.com>
2018-04-05 21:14:56 +0200

    Add files via upload

--- Assets/Script/DeadOnImpact.cs
+++ Assets/Script/DeadOnImpact.cs
@@ -7,10 +7,10 @@ public class DeadOnImpact : MonoBehaviour {
 
     // Use this for initialization
     void OnCollisionEnter2D(Collision2D col){
-        if (col.gameObject.name == "FloorOfDeath") {
+        if (col.gameObject.name == "Dead") {
             print("You Died");
             //transform.position = new Vector3(-1.14f, 0.86f, 0.0f);
-            SceneManager.LoadScene("GameOver");
+            SceneManager.LoadScene("Menu");
         }
     }
 }
