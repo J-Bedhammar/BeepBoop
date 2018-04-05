a8470c48bd5e42ef124b2f3ce3ff8279142f756e
Jennifer Bedhammar <jennifer95@hotmail.com>
2018-04-05 20:34:20 +0200

    Add files via upload
    New scripts huehue

---
+++ Assets/Script/DeadOnImpact.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeadOnImpact : MonoBehaviour {
+
+    // Use this for initialization
+    void OnCollisionEnter2D(Collision2D col){
+        if (col.gameObject.name == "FloorOfDeath") {
+            print("You Died");
+            //transform.position = new Vector3(-1.14f, 0.86f, 0.0f);
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
---
+++ Assets/Script/KeyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour {
+
+	// Use this for initialization
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            print("Right");
+            transform.Translate(1,0,0);
+        };
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            print("Left");
+            transform.Translate(-1,0,0);
+        };
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print("Left");
+            transform.Translate(0, 1, 0);
+        };
+    }
+}
